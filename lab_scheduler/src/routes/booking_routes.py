1c7bdfae0f4cbb2f6ec627a9a9aabdbd201d167a
PauloCosta30 <paulo.henriquee30@gmail.com>
2025-05-26 22:33:09 -0300

    booking_routes.py

--- lab_scheduler/src/routes/booking_routes.py
+++ lab_scheduler/src/routes/booking_routes.py
@@ -1,11 +1,9 @@
-# /home/ubuntu/lab_scheduler/src/routes/booking_routes.py
-
 from flask import Blueprint, request, jsonify, current_app, Response, make_response
 from src.extensions import db
 from src.models.entities import Room, Booking
 from datetime import datetime, date, time, timedelta, timezone
 from collections import defaultdict
-from flask_mail import Message # Import Message for Flask-Mail
+from flask_mail import Message  # Import Message for Flask-Mail
 # PDF Generation
 from weasyprint import HTML, CSS
 from jinja2 import Environment, FileSystemLoader
@@ -16,9 +14,9 @@ bookings_bp = Blueprint("bookings_bp", __name__)
 MAX_BOOKINGS_PER_DAY = 3
 
 # --- Booking Window Configuration ---
-CUTOFF_WEEKDAY = 2 # Wednesday
+CUTOFF_WEEKDAY = 2  # Wednesday
 CUTOFF_TIME = time(18, 0, 0, tzinfo=timezone.utc)
-RELEASE_WEEKDAY = 4 # Friday
+RELEASE_WEEKDAY = 4  # Friday
 RELEASE_TIME = time(0, 0, 0, tzinfo=timezone.utc)
 # ----------------------------------
 
@@ -43,8 +41,8 @@ def send_booking_confirmation_email(user_email, user_name, coordinator_name, boo
             booking_date_formatted = datetime.strptime(slot["booking_date"], "%Y-%m-%d").strftime("%d/%m/%Y")
         except ValueError:
             pass
-        html_body += f"<li>Sala: {slot["room_name"]} - Data: {booking_date_formatted} - Período: {slot["period"]}</li>"
-    html_body += f"</ul><p>Coordenador: {coordinator_name}</p><p>Obrigado!</p>"""
+        html_body += f"<li>Sala: {slot['room_name']} - Data: {booking_date_formatted} - Período: {slot['period']}</li>"
+    html_body += f"</ul><p>Coordenador: {coordinator_name}</p><p>Obrigado!</p>"
 
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.html = html_body
