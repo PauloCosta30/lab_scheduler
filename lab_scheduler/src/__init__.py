39a1001804b0093455c00b60ee975c5bfad62c4a
PauloCosta30 <paulo.henriquee30@gmail.com>
2025-07-05 23:41:18 -0300

    __init__.py

--- lab_scheduler/src/__init__.py
+++ lab_scheduler/src/__init__.py
@@ -1,29 +1,2 @@
-from flask import Flask
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object('config.Config')
 
-    # Configurar o nível de log para DEBUG
-    app.logger.setLevel(logging.DEBUG) 
-
-    db.init_app(app)
-    migrate.init_app(app, db)
-    mail.init_app(app)
-
-    from src.routes.booking_routes import bookings_bp
-    app.register_blueprint(bookings_bp, url_prefix='/api')
-
-    # Importar e registrar o blueprint de admin
-    from src.routes.admin_routes import admin_bp
-    app.register_blueprint(admin_bp, url_prefix='/admin')
-
-    with app.app_context():
-        db.create_all() # Cria as tabelas se não existirem
-        # Inicializa as salas padrão se o banco de dados estiver vazio
-        from src.models.entities import Room
-        if Room.query.count() == 0:
-            Room.initialize_default_rooms()
-            app.logger.info("Database initialized and custom rooms created.")
-    
-    return app
 
